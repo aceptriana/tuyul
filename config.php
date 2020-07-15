64be487b2b5b5324ac85be55a1724425ab822fb6
Tekaje Hacker Rulez <49948930+aceptriana@users.noreply.github.com>
2020-07-15 12:39:27 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$auth = "BASIC ZDM3MmJiMDJmYTY5NDY3NDg1ZTgxNTkzMDZiMjU4YTQ6NTNmOTc2M2U2NDg1MDE4MTgwZTlkZjY2MjlhNGUwMzg=";
+$ts = "1594620467840";
+$index = "20";
+$cookie = "u=73092962;n=fffffffff3a5827c2eb548e32eb548e3";
+
+?>
